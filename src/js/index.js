95d7be027f7201eb9003d9610da7b5cec23c6486
Lydie Paulo <lydie.paulo@abcsalles.com>
2021-04-28 09:08:46 +0200

    Remplissage des coeurs au clic

--- src/js/index.js
+++ src/js/index.js
@@ -15,100 +15,94 @@ function secondsToHms(duration) {
 }
 
 document.querySelector("#search-button")
-    .addEventListener("click", () => {
-        if (searchBar.value) {
-            window.fetch(`https://api.deezer.com/search?q=${searchBar.value}&order=${searchMode.value}`) //comment lier au résultat de la recherche ?
-                .then(response => response.json())
-                .then(result => {
-                    const resultData = result.data;
-                    //création du h2 "Résultats"
-                    const resDataLength = resultData.length;
+.addEventListener("click", () => {
+    if (searchBar.value) {
+        window.fetch(`https://api.deezer.com/search?q=${searchBar.value}&order=${searchMode.value}`) //comment lier au résultat de la recherche ?
+            .then(response => response.json())
+            .then(result => {
+                const resultData = result.data;
+                //création du h2 "Résultats"
+                const resDataLength = resultData.length;
 
-                    //si recherche sans résultat ou mauvaise recherche
-                    if (resDataLength == 0) {
-                        resH2.innerHTML = `<h2>Aucun résultat</h2>`;
+                //si recherche sans résultat ou mauvaise recherche
+                if (resDataLength == 0) {
+                    resH2.innerHTML = `<h2>Aucun résultat</h2>`;
+                }
+                else {
+                    //afficher nombre de résultats
+                    if (resDataLength < 2) {
+                        resH2.innerHTML = `<h2>${resDataLength} Résultat</h2>`;
                     }
                     else {
-                        //afficher nombre de résultats
-                        if (resDataLength < 2) {
-                            resH2.innerHTML = `<h2>${resDataLength} Résultat</h2>`;
-                        }
-                        else {
-                            resH2.innerHTML = `<h2>${resDataLength} Résultats</h2>`;
-                        }
+                        resH2.innerHTML = `<h2>${resDataLength} Résultats</h2>`;
                     }
+                }
 
-                    document.querySelector('#results').insertBefore(resH2, cardsList);
+                document.querySelector('#results').insertBefore(resH2, cardsList);
 
-                    //création des cartes pour chaque résultat
-                    cardsList.innerHTML = '';
-                    searchError.innerHTML = '';
+                //création des cartes pour chaque résultat
+                cardsList.innerHTML = '';
+                searchError.innerHTML = '';
 
-                    for (let i = 0; i < resDataLength; i++) {
-                        let newCard = document.createElement("div");
-                        let newCardLinks = document.createElement("div");
-                        let newFigure = document.createElement("figure");
-                        let newFigureCover = document.createElement("img");
-                        newFigureCover.setAttribute("src", resultData[i].album.cover_big);
-                        let newFigCaption = document.createElement("figcaption");
+                for (let i = 0; i < resDataLength; i++) {
+                    let newCard = document.createElement("div");
+                    let newCardLinks = document.createElement("div");
+                    let newFigure = document.createElement("figure");
+                    let newFigureCover = document.createElement("img");
+                    newFigureCover.setAttribute("src", resultData[i].album.cover_big);
+                    let newFigCaption = document.createElement("figcaption");
 
-                        cardsList.appendChild(newCard); //on crée la nouvelle card dans #cards-list
-                        newCard.appendChild(newCardLinks); //on crée div parent des liens dans la card
-                        newCard.appendChild(newFigure); //on crée figure dans la card
-                        newFigure.appendChild(newFigureCover); //on ajoute l'image de l'album dans figure
-                        newFigure.appendChild(newFigCaption); //on ajoute figcaption dans figure
+                    cardsList.appendChild(newCard); //on crée la nouvelle card dans #cards-list
+                    newCard.appendChild(newCardLinks); //on crée div parent des liens dans la card
+                    newCard.appendChild(newFigure); //on crée figure dans la card
+                    newFigure.appendChild(newFigureCover); //on ajoute l'image de l'album dans figure
+                    newFigure.appendChild(newFigCaption); //on ajoute figcaption dans figure
 
-                        newCardLinks.innerHTML += `
-                            <a href="pages/track.html?id=${resultData[i].id}"></a>
-                        `;
 
-                        const $favoriteTrack = document.createElement('button');
+                    let trackId = resultData[i].id;
 
-                        $favoriteTrack.onclick = () => {
-                            $favoriteTrack.style.fontWeight = "900";
+                    newCardLinks.innerHTML += `
+                        <a href="pages/track.html?id=${trackId}"></a>
+                    `;
 
-                            //on récupère les données existantes
-                            let trackList = localStorage.getItem('trackId');
+                    const $favoriteTrack = document.createElement('button');
 
-                            //s'il n'y en a pas on crée un tableau
-                            //s'il y en a, on transforme la string en tableau
-                            trackList = trackList ? trackList.split(',') : [];
+                    $favoriteTrack.addEventListener("click", () => {
+                        $favoriteTrack.style.cssText = "font-weight: 900; color: #e3502b"; //on remplit les cœurs au clic
 
-                            //ajout de nouvelles données au tableau
-                            trackList.push(resultData[i].id);
+                        //on récupère les données existantes
+                        let trackList = localStorage.getItem('trackIds');
 
-                            //on enregistre dans localstorage
-                            localStorage.setItem('trackId', trackList.toString());
+                        //s'il n'y en a pas on crée un tableau | s'il y en a, on transforme la string en tableau
+                        trackList = trackList ? trackList.split(',') : [];
 
-                            /*
-                            let clicked = false;
-                            if (clicked == false) {
-                                document.querySelector("#favorite").style.fontWeight = "400";
-                            }
-                            else {
-                                let clicked = false;
-                                localStorage.removeItem(resultData[i].id);
-                            } */
-                        };
+                        //on vérifie si l'id est déjà dans le tableau. si oui on l'enlève | sinon on l'ajoute
+                        /* trackList.push(trackId); */
+                        console.log(trackList.includes(trackId));
+                        trackList.includes(trackId) ? trackList.removeItem(trackId) : trackList.push(trackId);
 
-                        newFigure.appendChild($favoriteTrack);
+                        //on enregistre dans localstorage
+                        localStorage.setItem('trackIds', trackList.toString());
+                    });
 
-                        let durationToHms = secondsToHms(resultData[i].duration); //on passe duration en heures/minutes/secondes
+                    newFigure.appendChild($favoriteTrack);
 
-                        newFigCaption.innerHTML += `
-                            <h3>${resultData[i].title_short}</h3>
-                            <span><a href="pages/artist.html?id=${resultData[i].artist.id}">${resultData[i].artist.name}</a> / <a href="pages/album.html?id=${resultData[i].album.id}">${resultData[i].album.title}</a></span>
-                            <span>${durationToHms}</span>
-                        `;
-                    }
-                })
-            /* .catch(err => {
-            window.alert("La recherche n'a pas abouti !"); //gestion des erreurs
-            }); */
-        }
-        else {
-            searchError.innerHTML = `<h2>Aucun résultat</h2>`; //erreur si recherche vide
-            cardsList.innerHTML = '';
-            resH2.innerHTML = '';
-        }
-    });
+                    let durationToHms = secondsToHms(resultData[i].duration); //on passe duration en heures/minutes/secondes
+
+                    newFigCaption.innerHTML += `
+                        <h3>${resultData[i].title_short}</h3>
+                        <span><a href="pages/artist.html?id=${resultData[i].artist.id}">${resultData[i].artist.name}</a> / <a href="pages/album.html?id=${resultData[i].album.id}">${resultData[i].album.title}</a></span>
+                        <span>${durationToHms}</span>
+                    `;
+                }
+            })
+        /* .catch(err => {
+        window.alert("La recherche n'a pas abouti !"); //gestion des erreurs
+        }); */
+    }
+    else {
+        searchError.innerHTML = `<h2>Aucun résultat</h2>`; //erreur si recherche vide
+        cardsList.innerHTML = '';
+        resH2.innerHTML = '';
+    }
+});
